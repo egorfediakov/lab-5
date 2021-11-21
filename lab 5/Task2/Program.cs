54e298577f54e2d12e4d854c62f89d9cd3fe78ae
Valeriia Spasibukhova <megalera1913@gmail.com>
2021-11-21 23:50:03 +0200

    Добавьте файлы проекта.

---
+++ lab 5/Task2/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cone[] allkon = new Cone[2];
+            Console.WriteLine("Enter the radius and height of the cone: ");
+            try
+            {
+                double rK = Convert.ToDouble(Console.ReadLine());
+                double hK = Convert.ToDouble(Console.ReadLine());
+                allkon[0] = new Cone(rK, hK);
+                allkon[1] = new Frustum(rK, hK, 1.3);
+                Console.Write("\n");
+                foreach (Cone konuses in allkon)
+                {
+                    Console.WriteLine(konuses.ToString());
+                    Console.Write("\n");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Возникло исключение..");
+            }
+
+            Console.ReadKey();
+        }
+    }
+    class Cone
+    {
+        public double Height { get; set; }
+        public double Radius { get; set; }
+        public readonly string CName;
+
+        public Cone(double h, double r)
+        {
+            this.Height = h;
+            this.Radius = r;
+            CName = "Conus";
+        }
+        public virtual double ConVolume()
+        {
+            double vol = (Math.PI * Math.Pow(Radius, 2) * Height) / 3;
+            return vol;
+        }
+        public double ConArea()
+        {
+            double area = Math.PI * Math.Pow(Radius, 2);
+            return area;
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine($"{CName}: ");
+            Console.WriteLine($"Radius = {Radius}, Height = {Height}");
+            return $"Volume = {ConVolume()}, Area = {ConArea()}";
+        }
+    }
+    class Frustum : Cone
+    {
+        public double Radius2 { get; set; }
+        public readonly string FName;
+        public Frustum(double r, double h, double r2)
+            : base(r, h)
+        {
+            this.Radius2 = r2;
+            FName = "Frustum";
+        }
+
+        public override double ConVolume()
+        {
+            double vol1 = Math.PI * Height * (Math.Pow(Radius, 2) + (Radius * Radius2) + Math.Pow(Radius2, 2));
+            return vol1;
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine($"{FName}: ");
+            return $"Second Radius = {Radius2}, Volume {ConVolume()}";
+        }
+    }
+}
---
+++ lab 5/Task3/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the value for the length of the matrix: ");
+                int s1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the value for the width of the matrix: ");
+                int s2 = Convert.ToInt32(Console.ReadLine());
+                int[,] massive = new int[s1, s2];
+                new IdentMatrix(s1, s2, massive).IdentityMatrix();
+                new UpperMatrix(s1, s2, massive).UpperTrMatrix();
+            }
+            catch
+            {
+                Console.WriteLine("An exception was thrown");
+            }
+
+            Console.ReadKey();
+        }
+    }
+    public class Matrix
+    {
+        public int Size1 { get; set; }
+        public int Size2 { get; set; }
+        public int[,] MasMatrix { get; set; }
+
+        public Matrix(int s1, int s2, int[,] matrix)
+        {
+            this.Size1 = s1;
+            this.Size2 = s2;
+            this.MasMatrix = matrix;
+        }
+
+        public virtual void IdentityMatrix()
+        {
+
+            for (int i = 0; i < MasMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < MasMatrix.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        MasMatrix[i, j] = 1;
+                    }
+                    else
+                    {
+                        MasMatrix[i, j] = 0;
+                    }
+
+                    Console.Write(MasMatrix[i, j] + "\t");
+                }
+                Console.Write("\n");
+            }
+
+        }
+
+        public virtual void UpperTrMatrix()
+        {
+            for (int i = 0; i < MasMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < MasMatrix.GetLength(1); j++)
+                {
+                    MasMatrix[i, j] = 1;
+                }
+            }
+            for (int i = 0; i < MasMatrix.GetLength(0); i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < MasMatrix.GetLength(1); j++)
+                {
+
+                    if (i <= j)
+                        Console.Write(MasMatrix[i, j] + "\t");
+                    else
+                        Console.Write("0\t");
+                }
+
+            }
+        }
+
+    }
+    public class IdentMatrix : Matrix
+    {
+        public readonly string Name;
+        public IdentMatrix(int s1, int s2, int[,] massive)
+            : base(s1, s2, massive)
+        {
+            Name = "Indentity Matrix";
+        }
+        public override void IdentityMatrix()
+        {
+            Console.WriteLine($"{Name}: ");
+            base.IdentityMatrix();
+        }
+
+    }
+    public class UpperMatrix : Matrix
+    {
+        public readonly string Name;
+        public UpperMatrix(int s1, int s2, int[,] massive)
+            : base(s1, s2, massive)
+        {
+            Name = "Upper Triangular Matrix";
+        }
+
+        public override void UpperTrMatrix()
+        {
+            Console.WriteLine($"{Name}: ");
+            base.UpperTrMatrix();
+        }
+    }
+
+}
